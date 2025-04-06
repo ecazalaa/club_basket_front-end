be5e024c325e7a25303c1ac4551f0324424537a9
Romeo andriantsiferana <romeo.andriant@gmail.com>
2025-04-06 21:21:51 +0200

    derniere maj final

--- frontend/controleur/RechercheUtilisateur.php
+++ frontend/controleur/RechercheUtilisateur.php
@@ -10,7 +10,7 @@ class RechercheUtilisateur {
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->mdp = $mdp;
-        $this->apiUrl = "http://localhost:8888/siteBasketV2API/club_basket_apiAuth/authapi.php"; // Ajustez selon votre configuration
+        $this->apiUrl = "https://apiauthclubbasket.alwaysdata.net/club_basket_apiAuth/authapi.php"; // Ajustez selon votre configuration
         
     }
 
